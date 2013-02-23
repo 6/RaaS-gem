023b79db62cffee0c6de953b41b9cbe35e2c1bbb
Peter Graham <peterghm@gmail.com>
2013-02-23 00:00:45 -0800

    Move validations to separate methods

--- lib/RaaS.rb
+++ lib/RaaS.rb
@@ -15,10 +15,7 @@ module RaaS
   end
 
   def execute(method, options = {})
-    raise InvalidUrl  unless options[:url].present?
-    raise InvalidUrl  unless options[:url].starts_with?("http://", "https://")
-    raise InvalidEndpointUrl  unless options[:endpoint_url].present?
-    raise InvalidHttpMethod  unless [:get, :post].include?(method)
+    validate_params!(method, options)
 
     options[:method] = :post
     options[:url] = "#{options[:endpoint_url]}/#{method.to_s}?url=#{CGI.escape(options[:url])}"
@@ -29,11 +26,22 @@ module RaaS
     options.delete(:endpoint_url)
 
     RestClient::Request.execute(options) do |response, request, result, &block|
-      raise BadRequest, JSON.parse(response.body)['error']  if response.code == 400
-      raise InternalServerError  if response.code >= 500
-      raise UnexpectedStatusCode, response.code.to_s  if response.code != 200
+      validate_response!(response)
       JSON.parse(response.body)
     end
+  end
+
+  def validate_params!(method, options)
+    raise InvalidUrl  unless options[:url].present?
+    raise InvalidUrl  unless options[:url].starts_with?("http://", "https://")
+    raise InvalidEndpointUrl  unless options[:endpoint_url].present?
+    raise InvalidHttpMethod  unless [:get, :post].include?(method)
+  end
+
+  def validate_response!(response)
+    raise BadRequest, JSON.parse(response.body)['error']  if response.code == 400
+    raise InternalServerError  if response.code >= 500
+    raise UnexpectedStatusCode, response.code.to_s  if response.code != 200
   end
 end
 
